59f410673f0326a663f0aaba1b76dab4a21e33c4
Alexis Metaireau <ametaireau@gmail.com>
2010-08-18 15:10:43 +0200

    Add the notmyidea theme + settings as an example.

---
+++ samples/test_settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+BLOGNAME = 'NotMyIdea.org'
+BLOGSUBTITLE = u"Alexis Métaireau's weblog"
+BLOGURL = 'http://blog.notmyidea.org'
+
+BLOGROLL = (('Biologeek', 'http://biologeek.org'),
+            ('Filyb', "http://filyb.info/"),
+            ('Libert-fr', "http://www.libert-fr.com"),
+            ('N1k0', "http://prendreuncafe.com/blog/"),
+            (u'Tarek Ziadé', "http://ziade.org/blog"),
+            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+
+MENUITEMS = (('home', 'http://notmyidea.org'),
+             ('contact', 'mailto: alexis notmyidea org'),)
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),)
