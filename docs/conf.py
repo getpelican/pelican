934f1e41db6bfcb577523397012a2788232ab86b
Alexis Metaireau <ametaireau@gmail.com>
2010-10-29 02:30:51 +0100

    Theming :)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,11 @@ html_theme = 'flask_small'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'index_logo': 'pelican.gif',
+    'github_fork': 'ametaireau/pelican',
+    'index_logo_height': '200px',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
