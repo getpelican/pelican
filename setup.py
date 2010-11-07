01f2912c8f545a968f83c6a2d0fd2bf1d4c1642a
Alexis Metaireau <ametaireau@gmail.com>
2010-11-07 14:36:04 +0000

    bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (2,7):
 
 setup(
     name = "pelican",
-    version = '2.4.3',
+    version = '2.4.4',
     url = 'http://alexis.notmyidea.org/pelican/',
     author = 'Alexis Metaireau',
     author_email = 'alexis@notmyidea.org',
