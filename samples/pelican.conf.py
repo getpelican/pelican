f352d16971a6bceddc2318d2dca122dd5a41f9e3
Alexis Metaireau <alexis@notmyidea.org>
2010-12-05 23:11:45 +0000

    Put the real version of the conf file, not the symbolic link ;)

--- samples/pelican.conf.py
+++ samples/pelican.conf.py
@@ -1,1 +1,19 @@
-/home/alexis/projets/notmyidea.org/blog/pelican.conf.py
+# -*- coding: utf-8 -*-
+AUTHOR = u'Alexis Métaireau'
+SITENAME = u"Alexis' log" 
+SITEURL = 'http://blog.notmyidea.org'
+
+GITHUB_URL = 'http://github.com/ametaireau/'
+DISQUS_SITENAME = "blog-notmyidea"
+PDF_PROCESSOR = False
+
+LINKS = (('Biologeek', 'http://biologeek.org'),
+            ('Filyb', "http://filyb.info/"),
+            ('Libert-fr', "http://www.libert-fr.com"),
+            ('N1k0', "http://prendreuncafe.com/blog/"),
+            (u'Tarek Ziadé', "http://ziade.org/blog"),
+            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),
+          ('github', 'http://github.com/ametaireau'),)
