8454b0d1b8f160227ebc15f5c94724edd682d628
Alexis Metaireau <alexis@notmyidea.org>
2011-01-13 00:46:10 +0100

    Tests for settings.

--- pelican/settings.py
+++ pelican/settings.py
@@ -1,35 +1,35 @@
 import os
 
-_DEFAULT_THEME = os.sep.join([os.path.dirname(os.path.abspath(__file__)),
+DEFAULT_THEME = os.sep.join([os.path.dirname(os.path.abspath(__file__)),
                               "themes/notmyidea"])
-_DEFAULT_CONFIG = {'PATH': None,
-                   'THEME': _DEFAULT_THEME,
-                   'OUTPUT_PATH': 'output/',
-                   'MARKUP': ('rst', 'md'),
-                   'STATIC_PATHS': ['images',],
-                   'THEME_STATIC_PATHS': ['static',],
-                   'FEED': 'feeds/all.atom.xml',
-                   'CATEGORY_FEED': 'feeds/%s.atom.xml',
-                   'TRANSLATION_FEED': 'feeds/all-%s.atom.xml',
-                   'SITENAME': 'A Pelican Blog',
-                   'DISPLAY_PAGES_ON_MENU': True,
-                   'PDF_GENERATOR': False,
-                   'DEFAULT_CATEGORY': 'misc',
-                   'FALLBACK_ON_FS_DATE': True,
-                   'CSS_FILE': 'main.css',
-                   'REVERSE_ARCHIVE_ORDER': False,
-                   'KEEP_OUTPUT_DIRECTORY': False,
-                   'CLEAN_URLS': False, # use /blah/ instead /blah.html in urls
-                   'RELATIVE_URLS': True,
-                   'DEFAULT_LANG': 'en',
-                   'PELICAN_CLASS': 'pelican.Pelican',
-                   'JINJA_EXTENSIONS': [],
+DEFAULT_CONFIG = {'PATH': None,
+                  'THEME': DEFAULT_THEME,
+                  'OUTPUT_PATH': 'output/',
+                  'MARKUP': ('rst', 'md'),
+                  'STATIC_PATHS': ['images',],
+                  'THEME_STATIC_PATHS': ['static',],
+                  'FEED': 'feeds/all.atom.xml',
+                  'CATEGORY_FEED': 'feeds/%s.atom.xml',
+                  'TRANSLATION_FEED': 'feeds/all-%s.atom.xml',
+                  'SITENAME': 'A Pelican Blog',
+                  'DISPLAY_PAGES_ON_MENU': True,
+                  'PDF_GENERATOR': False,
+                  'DEFAULT_CATEGORY': 'misc',
+                  'FALLBACK_ON_FS_DATE': True,
+                  'CSS_FILE': 'main.css',
+                  'REVERSE_ARCHIVE_ORDER': False,
+                  'KEEP_OUTPUT_DIRECTORY': False,
+                  'CLEAN_URLS': False, # use /blah/ instead /blah.html in urls
+                  'RELATIVE_URLS': True,
+                  'DEFAULT_LANG': 'en',
+                  'PELICAN_CLASS': 'pelican.Pelican',
+                  'JINJA_EXTENSIONS': [],
                   }
 
 def read_settings(filename):
     """Load a Python file into a dictionary.
     """
-    context = _DEFAULT_CONFIG.copy()
+    context = DEFAULT_CONFIG.copy()
     if filename:
         tempdict = {}
         execfile(filename, tempdict)
--- samples/pelican.conf.py
+++ samples/pelican.conf.py
@@ -8,14 +8,18 @@ DISQUS_SITENAME = "blog-notmyidea"
 PDF_GENERATOR = False
 
 LINKS = (('Biologeek', 'http://biologeek.org'),
-            ('Filyb', "http://filyb.info/"),
-            ('Libert-fr', "http://www.libert-fr.com"),
-            ('N1k0', "http://prendreuncafe.com/blog/"),
-            (u'Tarek Ziadé', "http://ziade.org/blog"),
-            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+         ('Filyb', "http://filyb.info/"),
+         ('Libert-fr', "http://www.libert-fr.com"),
+         ('N1k0', "http://prendreuncafe.com/blog/"),
+         (u'Tarek Ziadé', "http://ziade.org/blog"),
+         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
 
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('lastfm', 'http://lastfm.com/user/akounet'),
           ('github', 'http://github.com/ametaireau'),)
 
 STATIC_PATHS = ["pictures",]
+
+# foobar will not be used, because it's not in caps. All configuration keys 
+# have to be in caps
+foobar = "barbaz"
