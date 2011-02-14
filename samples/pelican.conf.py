fe69a935b02ff53709f267eb33a91d2f91639e22
Laureline Guerin <laureline.guerin@makina-corpus.com>
2011-02-14 23:24:54 +0800

    Pagination added for index and tag/category pages

--- samples/pelican.conf.py
+++ samples/pelican.conf.py
@@ -8,6 +8,7 @@ DISQUS_SITENAME = "blog-notmyidea"
 PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
 LOCALE = 'fr_FR.utf8'
+DEFAULT_PAGINATION = 2
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
