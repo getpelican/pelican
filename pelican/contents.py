getpelican/pelican

commit 0b6896a0ab2866ce9a80deb69ee0677f0d178f0a

Adding a summary no more make Pelican crashes
Setting pelican.readers.Article.summary was impossible and raised an
exception because pelican.readers.Article.summary was a @property

pelican/contents.py
@@ -88,6 +88,10 @@ class Page(object):
         if not hasattr(self, 'status'):
             self.status = settings['DEFAULT_STATUS']
 
+        # set summary
+        if not hasattr(self, 'summary'):
+            self.summary = truncate_html_words(self.content, 50)
+
     def check_properties(self):
         """test that each mandatory property is set."""
         for prop in self.mandatory_properties:
@@ -102,9 +106,6 @@ class Page(object):
             content = self._content
         return content
 
-    @property
-    def summary(self):
-        return truncate_html_words(self.content, 50)
 
 class Article(Page):
     mandatory_properties = ('title', 'date', 'category')

commit b9157df1a3f30adf32d03003a42295dfc665fa4d

Fixes the bug re-introduced in 0b6896a. Thank to @mapio for noticing the problem :-)

pelican/contents.py
@@ -106,6 +106,18 @@ class Page(object):
             content = self._content
         return content
 
+    def _get_summary(self):
+        """Returns the summary of an article, based on to the content"""
+        return truncate_html_words(self.content, 50)
+
+    def _set_summary(self, summary):
+        """Dummy function"""
+        pass
+
+    summary = property(_get_summary, _set_summary, \
+                       "Summary of the article. Based on the content. Can't be set")
+
+
 
 class Article(Page):
     mandatory_properties = ('title', 'date', 'category')