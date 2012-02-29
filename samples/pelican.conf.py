917633628ac5e7dfb654fbfb9ce0963d3d3eb3a9
Alexis Metaireau <alexis@notmyidea.org>
2012-02-29 18:15:38 +0100

    add some more articles for the tests

--- samples/pelican.conf.py
+++ samples/pelican.conf.py
@@ -9,7 +9,7 @@ DISQUS_SITENAME = "blog-notmyidea"
 PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
 LOCALE = ""
-DEFAULT_PAGINATION = 2
+DEFAULT_PAGINATION = 4
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
