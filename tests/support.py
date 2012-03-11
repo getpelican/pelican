32355f546373a1dcc4faa4d426f71c4552642956
Alexis Metaireau <alexis@notmyidea.org>
2012-03-11 15:51:48 +0100

    add some more tests for the utils module

--- tests/support.py
+++ tests/support.py
@@ -1,7 +1,8 @@
 from contextlib import contextmanager
-
 from tempfile import mkdtemp
 from shutil import rmtree
+
+from pelican.contents import Article
 
 
 @contextmanager
@@ -16,3 +17,10 @@ def temporary_folder():
     tempdir = mkdtemp()
     yield tempdir
     rmtree(tempdir)
+
+
+def get_article(title, slug, content, lang, extra_metadata=None):
+    metadata = {'slug': slug, 'title': title, 'lang': lang}
+    if extra_metadata is not None:
+        metadata.update(extra_metadata)
+    return Article(content, metadata=metadata)
