ec31832c5cf5f1bdfcf42a6719648c7c5efdffb2
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-03-14 20:04:58 +0000

    - reorganize imports and remove unused import - remove trailing spaces at the end of the generated files

--- tools/pelican_quickstart.py
+++ tools/pelican_quickstart.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-import os, sys, argparse, string
+import os
+import string
+import argparse
+
 from pelican import __version__
 
-TEMPLATES={
+TEMPLATES = {
     'Makefile' : '''
 PELICAN=$pelican
 PELICANOPTS=$pelicanopts
@@ -60,7 +63,7 @@ github: $$(OUTPUTDIR)/index.html
 \tgit push origin gh-pages
 
 .PHONY: html help clean ftp_upload ssh_upload dropbox_upload github
-    ''',
+''',
 
     'pelican.conf.py': '''#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
@@ -87,9 +90,7 @@ SOCIAL = (
          )
 
 DEFAULT_PAGINATION = $default_pagination
-
-
-    '''
+'''
 }
 
 CONF = {
