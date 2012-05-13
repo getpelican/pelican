027c51659ce8377e5188686b3a56ecbb4be4c904
sam <lesametlemax@gmail.com>
2012-05-13 22:44:59 +0200

    Removed strip_raw code

--- pelican/tools/pelican_import.py
+++ pelican/tools/pelican_import.py
@@ -203,7 +203,7 @@ def build_markdown_header(title, date, author, categories, tags):
     header += '\n'
     return header
 
-def fields2pelican(fields, out_markup, output_path, dircat=False, strip_raw=False):
+def fields2pelican(fields, out_markup, output_path, dircat=False):
     for title, content, filename, date, author, categories, tags, in_markup in fields:
         if (in_markup == "markdown") or (out_markup == "markdown") :
             ext = '.md'
@@ -238,10 +238,9 @@ def fields2pelican(fields, out_markup, output_path, dircat=False, strip_raw=Fals
 
                 fp.write(content)
 
-            parse_raw = '' if not strip_raw else '--parse-raw'
-            cmd = ('pandoc --normalize --reference-links {0} --from=html'
-                   ' --to={1} -o "{2}" "{3}"').format(
-                    parse_raw, out_markup, out_filename, html_filename)
+            cmd = ('pandoc --normalize --reference-links --from=html'
+                   ' --to={0} -o "{1}" "{2}"').format(
+                   out_markup, out_filename, html_filename)
 
             try:
                 rc = subprocess.call(cmd, shell=True)
@@ -287,9 +286,6 @@ def main():
         help='Output markup format (supports rst & markdown)')
     parser.add_argument('--dir-cat', action='store_true', dest='dircat',
         help='Put files in directories with categories name')
-    parser.add_argument('--strip-raw', action='store_true', dest='strip_raw',
-        help="Strip raw HTML code that can't be converted to "
-             "markup such as flash embeds or iframes (wordpress import only)")
 
     args = parser.parse_args()
 
@@ -318,6 +314,4 @@ def main():
     elif input_type == 'feed':
         fields = feed2fields(args.input)
 
-    fields2pelican(fields, args.markup, args.output,
-                   dircat=args.dircat or False,
-                   strip_raw=args.strip_raw or False)
+    fields2pelican(fields, args.markup, args.output, dircat=args.dircat or False)
