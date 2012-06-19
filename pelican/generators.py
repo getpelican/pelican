f10529df89ebeb315464339a5e936efe86153d23
John Mastro <john.b.mastro@gmail.com>
2012-06-19 21:31:08 +0000

    Fix a bug in .generate_period_archives()
    Identify months via a (year, month) tuple.

--- pelican/generators.py
+++ pelican/generators.py
@@ -184,34 +184,36 @@ class ArticlesGenerator(Generator):
         month_save_as = self.settings.get('MONTH_ARCHIVE_SAVE_AS')
         day_save_as = self.settings.get('DAY_ARCHIVE_SAVE_AS')
 
-        # `self.dates` is sorted based on REVERSE_ARCHIVE_ORDER
-        dates = set([a.date for a in self.dates])
+        # Use `self.dates` rather than `self.articles` since it's
+        # sorted based on the REVERSE_ARCHIVE_ORDER setting.
+        archives = [a for a in self.dates if a in self.articles]
+        article_dates = {a.date for a in archives}
+
+        get_month = attrgetter('year', 'month')
 
         prev_year = None
         prev_month = None
-        for date in dates:
-            year, month = date.year, date.month
+        for date in article_dates:
+            year = date.year
+            month = get_month(date)
             if year_save_as and year != prev_year:
                 # Pass the full `datetime.date` object for formatting
-                # `save_as`. The directives specific to the day, minute,
-                # etc aren't appropriate here (or for the month archive),
-                # but this way the user can employ the same syntax as
-                # for article URLs.
+                # `save_as` so that the user can employ the same syntax
+                # as for article URLs.
                 save_as = year_save_as.format(date=date)
-                articles = [a for a in self.dates if a.date.year == year]
+                articles = [a for a in archives if a.date.year == year]
                 write(save_as, template, self.context,
                       articles=articles, blog=True)
                 prev_year = year
             if month_save_as and month != prev_month:
                 save_as = month_save_as.format(date=date)
-                articles = [a for a in self.dates if a.date.year == year
-                            and a.date.month == month]
+                articles = [a for a in archives if get_month(a.date) == month]
                 write(save_as, template, self.context,
                       articles=articles, blog=True)
                 prev_month = month
             if day_save_as:
                 save_as = day_save_as.format(date=date)
-                articles = [a for a in self.dates if a.date == date]
+                articles = [a for a in archives if a.date == date]
                 write(save_as, template, self.context,
                       articles=articles, blog=True)
 
