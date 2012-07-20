597dba1391a8b8d7d827269cfc9022786398e7f5
Joshua Adelman <joshua.adelman@gmail.com>
2012-07-19 20:59:48 -0400

    Added signals to plugin for pages generation

--- pelican/signals.py
+++ pelican/signals.py
@@ -3,3 +3,5 @@ from blinker import signal
 initialized = signal('pelican_initialized')
 article_generate_context = signal('article_generate_context')
 article_generator_init = signal('article_generator_init')
+pages_generate_context = signal('pages_generate_context')
+pages_generator_init = signal('pages_generator_init')
