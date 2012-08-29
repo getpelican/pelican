0acc401aa7d9953da5d50a93231fa65bb999c4e3
Dirk Makowski <dmakowski@gmx.net>
2012-08-29 17:08:08 +0200

    Added Django's decorator python_2_unicode_compatible

--- pelican/utils.py
+++ pelican/utils.py
@@ -14,6 +14,24 @@ from operator import attrgetter
 
 logger = logging.getLogger(__name__)
 
+#----------------------------------------------------------------------------
+# Stolen from Django: django.utils.encoding
+#
+
+def python_2_unicode_compatible(klass):
+    """
+    A decorator that defines __unicode__ and __str__ methods under Python 2.
+    Under Python 3 it does nothing.
+
+    To support Python 2 and 3 with a single code base, define a __str__ method
+    returning text and apply this decorator to the class.
+    """
+    if not six.PY3:
+        klass.__unicode__ = klass.__str__
+        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
+    return klass
+
+#----------------------------------------------------------------------------
 
 def get_date(string):
     """Return a datetime object from a string.
