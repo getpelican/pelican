b6cc6bd2d4c2842c0856726caee649acaf3642f0
Bruno Binet <bruno.binet@gmail.com>
2012-10-25 14:24:13 +0200

    remove useless try/except clause
    exception will be caught and logged later in Pelican stack

--- pelican/utils.py
+++ pelican/utils.py
@@ -87,15 +87,13 @@ def copy(path, source, destination, destination_path=None, overwrite=False):
             if overwrite:
                 shutil.rmtree(destination_)
                 shutil.copytree(source_, destination_)
-                logger.info('replacement of %s with %s' % (source_, destination_))
+                logger.info('replacement of %s with %s' % (source_,
+                    destination_))
 
     elif os.path.isfile(source_):
-        try:
-            dest_dir = os.path.dirname(destination_)
-            if not os.path.exists(dest_dir):
-                os.makedirs(dest_dir)
-        except OSError, e:
-            logger.error("copying: %s -> %s: %r" % (source_, dest_dir, e))
+        dest_dir = os.path.dirname(destination_)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
         shutil.copy(source_, destination_)
         logger.info('copying %s to %s' % (source_, destination_))
 
