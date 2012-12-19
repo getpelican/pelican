6ade6a2eea810b2de49677db23e9f6db43a07814
James King <james@agentultra.com>
2012-12-18 21:39:46 -0500

    Refactor test_importer
    All of the tests were coercing the wordpress fields to list so I just
    do it in setUp. DRY, etc.

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -20,40 +20,54 @@ except ImportError:
 class TestWordpressXmlImporter(unittest.TestCase):
 
     def setUp(self):
-        self.posts = wp2fields(WORDPRESS_XML_SAMPLE)
+        self.posts = list(wp2fields(WORDPRESS_XML_SAMPLE))
 
     def test_ignore_empty_posts(self):
-
-        posts = list(self.posts)
-        self.assertTrue(posts)
+        self.assertTrue(self.posts)
         for title, content, fname, date, author, categ, tags, format in posts:
             self.assertTrue(title.strip())
 
     def test_can_toggle_raw_html_code_parsing(self):
-
-        posts = list(self.posts)
         r = lambda f: open(f).read()
         silent_f2p = mute(True)(fields2pelican)
 
         with temporary_folder() as temp:
 
-            rst_files = (r(f) for f in silent_f2p(posts, 'markdown', temp))
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'markdown', temp))
             self.assertTrue(any('<iframe' in rst for rst in rst_files))
-            rst_files = (r(f) for f in silent_f2p(posts, 'markdown', temp,
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'markdown', temp,
                          strip_raw=True))
             self.assertFalse(any('<iframe' in rst for rst in rst_files))
             # no effect in rst
-            rst_files = (r(f) for f in silent_f2p(posts, 'rst', temp))
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'rst', temp))
             self.assertFalse(any('<iframe' in rst for rst in rst_files))
-            rst_files = (r(f) for f in silent_f2p(posts, 'rst', temp,
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'rst', temp,
                          strip_raw=True))
             self.assertFalse(any('<iframe' in rst for rst in rst_files))
+
+    def test_can_toggle_slug_storage(self):
+        r = lambda f: open(f).read()
+        silent_f2p = mute(True)(fields2pelican)
+
+        with temporary_folder() as temp:
+
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'markdown', temp))
+            self.assertTrue(all('Slug:' in rst for rst in rst_files))
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'markdown', temp,
+                         disable_slugs=True))
+            self.assertFalse(any('Slug:' in rst for rst in rst_files))
+
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'rst', temp))
+            self.assertTrue(all(':slug:' in rst for rst in rst_files))
+            rst_files = (r(f) for f in silent_f2p(self.posts, 'rst', temp,
+                         disable_slugs=True))
+            self.assertFalse(any(':slug:' in rst for rst in rst_files))
 
     def test_decode_html_entities_in_titles(self):
         posts = list(self.posts)
         test_posts = [post for post in posts if post[2] == 'html-entity-test']
         self.assertTrue(len(test_posts) == 1)
-        
+
         post = test_posts[0]
         title = post[0]
         self.assertTrue(title, "A normal post with some <html> entities in the title. You can't miss them.")
