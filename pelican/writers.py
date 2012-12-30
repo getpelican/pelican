e07b39dfcb4748066ade21ed75f41295ecd8ac7e
Ross McFarland <rwmcfa1@gmail.com>
2012-12-29 17:44:35 -0800

    more robust PAGINATION_(URL|SAVE_AS) support
    - add base_name and number_seperator to context to give more flexibility
      when naming things

--- pelican/writers.py
+++ pelican/writers.py
@@ -150,7 +150,7 @@ class Writer(object):
         # check paginated
         paginated = paginated or {}
         if paginated:
-            name_root, ext = os.path.splitext(name)
+            name_root = os.path.splitext(name)[0]
 
             # pagination needed, init paginators
             paginators = {}
