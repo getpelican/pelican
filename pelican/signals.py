effe7e5e126343e9b787d0434e79599c499b8443
W. Trevor King <wking@tremily.us>
2013-01-04 18:53:12 -0500

    signals: Sort signals into categories

--- pelican/signals.py
+++ pelican/signals.py
@@ -2,15 +2,27 @@
 from __future__ import unicode_literals, print_function
 from blinker import signal
 
+# Run-level signals:
+
 initialized = signal('pelican_initialized')
+get_generators = signal('get_generators')
 finalized = signal('pelican_finalized')
-article_generate_preread = signal('article_generate_preread')
+
+# Generator-level signals
+
 generator_init = signal('generator_init')
-article_generate_context = signal('article_generate_context')
+
 article_generator_init = signal('article_generator_init')
 article_generator_finalized = signal('article_generate_finalized')
-get_generators = signal('get_generators')
-pages_generate_context = signal('pages_generate_context')
+
 pages_generator_init = signal('pages_generator_init')
 pages_generator_finalized = signal('pages_generator_finalized')
+
+# Page-level signals
+
+article_generate_preread = signal('article_generate_preread')
+article_generate_context = signal('article_generate_context')
+
+pages_generate_context = signal('pages_generate_context')
+
 content_object_init = signal('content_object_init')
