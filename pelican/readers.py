02f86994022a35c192af9cb3c8dbc26cfbe6eb65
W. Trevor King <wking@tremily.us>
2013-01-13 19:40:48 -0500

    readers: Use regexp groups to match HTML metadata
    The new regexp is also more robust.  For example, it allows for colons
    and non-ASCII characters in the value.

--- pelican/readers.py
+++ pelican/readers.py
@@ -193,18 +193,15 @@ class MarkdownReader(Reader):
 
 class HtmlReader(Reader):
     file_extensions = ['html', 'htm']
-    _re = re.compile('\<\!\-\-\#\s?[A-z0-9_-]*\s?\:s?[A-z0-9\s_-]*\s?\-\-\>')
+    _re = re.compile('<!--#\s*([A-z0-9_-]*)\s*:\s*([^>]*?)\s*-->')
 
     def read(self, source_path):
         """Parse content and metadata of (x)HTML files"""
         with open(source_path) as content:
             metadata = {'title': 'unnamed'}
-            for i in self._re.findall(content):
-                key = i.split(':')[0][5:].strip()
-                value = i.split(':')[-1][:-3].strip()
+            for key,value in self._re.findall(content):
                 name = key.lower()
                 metadata[name] = self.process_metadata(name, value)
-
             return content, metadata
 
 
