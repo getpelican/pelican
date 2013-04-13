c4b3ad58e8c1f23501e881801dad60daf266e8d2
Deniz Turgut <dturgut@gmail.com>
2013-04-12 23:39:39 -0400

    remove plugins, update docs and update dependecies

--- pelican/plugins/initialized.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function
-from pelican import signals
-
-def test(sender):
-    print("%s initialized !!" % sender)
-
-def register():
-    signals.initialized.connect(test)
