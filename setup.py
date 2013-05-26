529d0d99c5d5789bf3b91c7235fcad89c44bd108
Simon Conseil <contact@saimon.org>
2013-05-26 11:18:34 +0200

    Update version requirement with feedgenerator 1.6
    feedgenerator 1.6 changes the order of attributes in feeds, so we must use this
    version to have consistent functionnal tests.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-requires = ['feedgenerator>=1.5', 'jinja2 >= 2.6', 'pygments', 'docutils',
+requires = ['feedgenerator >= 1.6', 'jinja2 >= 2.6', 'pygments', 'docutils',
             'pytz', 'blinker', 'unidecode', 'six']
 
 try:
