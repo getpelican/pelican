wking/swc-website

generators: Update PagesGenerator to use new read_file
Also standardize signal names.  If `article_generator_*` is singular,
`page_generator_*` should be as well.  Fix it from the older
`pages_generator_*`.

pelican/signals.py
@@ -13,8 +13,8 @@ generator_init = signal('generator_init')
 article_generator_init = signal('article_generator_init')
 article_generator_finalized = signal('article_generate_finalized')
 
-pages_generator_init = signal('pages_generator_init')
-pages_generator_finalized = signal('pages_generate_finalized')
+page_generator_init = signal('page_generator_init')
+page_generator_finalized = signal('page_generate_finalized')
 
 static_generator_init = signal('static_generator_init')
 static_generator_finalized = signal('static_generate_finalized')
@@ -24,8 +24,8 @@ static_generator_finalized = signal('static_generate_finalized')
 article_generator_preread = signal('article_generator_preread')
 article_generator_context = signal('article_generator_context')
 
-pages_generator_preread = signal('pages_generator_preread')
-pages_generator_context = signal('pages_generator_context')
+page_generator_preread = signal('page_generator_preread')
+page_generator_context = signal('page_generator_context')
 
 static_generator_preread = signal('static_generator_preread')
 static_generator_context = signal('static_generator_context')

getpelican/pelican

pelican/signals.py
@@ -15,8 +15,8 @@ generator_init = signal('generator_init')
 article_generator_init = signal('article_generator_init')
 article_generator_finalized = signal('article_generator_finalized')
 
-pages_generator_init = signal('pages_generator_init')
-pages_generator_finalized = signal('pages_generator_finalized')
+page_generator_init = signal('page_generator_init')
+page_generator_finalized = signal('page_generator_finalized')
 
 static_generator_init = signal('static_generator_init')
 static_generator_finalized = signal('static_generator_finalized')
@@ -26,8 +26,8 @@ static_generator_finalized = signal('static_generator_finalized')
 article_generator_preread = signal('article_generator_preread')
 article_generator_context = signal('article_generator_context')
 
-pages_generator_preread = signal('pages_generator_preread')
-pages_generator_context = signal('pages_generator_context')
+page_generator_preread = signal('page_generator_preread')
+page_generator_context = signal('page_generator_context')
 
 static_generator_preread = signal('static_generator_preread')
 static_generator_context = signal('static_generator_context')