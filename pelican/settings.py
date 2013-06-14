getpelican/pelican

commit 1d4d86c876f6a2b29a850212d915b3a0139c4a88

settings: Rework the LESS_GENERATOR removal warning for easy extension
Setting migration is something that will happen for multiple settings,
and the more we can standardize the reporting procedure, the easier it
will be to add new warnings.

pelican/settings.py
@@ -210,11 +210,6 @@ def configure_settings(settings):
             'http://docs.getpelican.com/en/latest/settings.html#timezone '
             'for more information')
 
-    if 'LESS_GENERATOR' in settings:
-        logger.warning(
-            'The LESS_GENERATOR setting has been removed in favor '
-            'of the Webassets plugin')
-
     if 'OUTPUT_SOURCES_EXTENSION' in settings:
         if not isinstance(settings['OUTPUT_SOURCES_EXTENSION'],
                           six.string_types):
@@ -255,5 +250,14 @@ def configure_settings(settings):
                         "be a list), falling back to the default"
                         % PATH_KEY)
                 settings[PATH_KEY] = DEFAULT_CONFIG[PATH_KEY]
+
+    for old,new,doc in [
+            ('LESS_GENERATOR', 'the Webassets plugin', None),
+            ]:
+        if old in settings:
+            message = 'The {} setting has been removed in favor of {}'
+            if doc:
+                message += ', see {} for details'
+            logger.warning(message)
 
     return settings

commit 180cf9165fd0f35d57c51b4cc4111f1a5a92858c

settings: Fix deprecation warning in configure_settings()
The broken code came from my 1d4d86c (settings: Rework the
LESS_GENERATOR removal warning for easy extension, 2013-03-24), where
I put formatting placeholders ({}) into the warning message, but
forgot to fill them in :/.

pelican/settings.py
@@ -261,9 +261,10 @@ def configure_settings(settings):
              'https://github.com/getpelican/pelican/blob/master/docs/settings.rst#path-metadata'),
             ]:
         if old in settings:
-            message = 'The {} setting has been removed in favor of {}'
+            message = 'The {} setting has been removed in favor of {}'.format(
+                old, new)
             if doc:
-                message += ', see {} for details'
+                message += ', see {} for details'.format(doc)
             logger.warning(message)
 
     return settings