89a31141ff51377ca97cde0f3da2f3651f46079d
Tom Adler <me@arty.name>
2014-07-13 08:59:09 +0200

    Support last page pattern in PAGINATION_PATTERNS

--- pelican/tests/test_paginator.py
+++ pelican/tests/test_paginator.py
@@ -76,3 +76,29 @@ class TestPage(unittest.TestCase):
         page2 = paginator.page(2)
         self.assertEqual(page2.save_as, 'blog/2/index.html')
         self.assertEqual(page2.url, '//blog.my.site/2/')
+
+    def test_custom_pagination_pattern_last_page(self):
+        from pelican.paginator import PaginationRule
+        settings = get_settings()
+        settings['PAGINATION_PATTERNS'] = [PaginationRule(*r) for r in [
+            (1, '/{url}1/', '{base_name}/1/index.html'),
+            (2, '/{url}{number}/', '{base_name}/{number}/index.html'),
+            (-1, '/{url}', '{base_name}/index.html'),
+        ]]
+
+        self.page_kwargs['metadata']['author'] = Author('Blogger', settings)
+        object_list = [Article(**self.page_kwargs),
+                       Article(**self.page_kwargs),
+                       Article(**self.page_kwargs)]
+        paginator = Paginator('blog/index.html', '//blog.my.site/',
+                              object_list, settings, 1)
+        # The URL *has to* stay absolute (with // in the front), so verify that
+        page1 = paginator.page(1)
+        self.assertEqual(page1.save_as, 'blog/1/index.html')
+        self.assertEqual(page1.url, '//blog.my.site/1/')
+        page2 = paginator.page(2)
+        self.assertEqual(page2.save_as, 'blog/2/index.html')
+        self.assertEqual(page2.url, '//blog.my.site/2/')
+        page3 = paginator.page(3)
+        self.assertEqual(page3.save_as, 'blog/index.html')
+        self.assertEqual(page3.url, '//blog.my.site/')
