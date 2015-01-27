4f466bffebd4b0c77072bc1f9687268c84facd13
Jotham Apaloo <jothamapaloo@gmail.com>
2015-01-26 22:55:52 -0500

    Make working link to pandoc templates.

--- pelican/settings.py
+++ pelican/settings.py
@@ -135,7 +135,6 @@ DEFAULT_CONFIG = {
     'LOAD_CONTENT_CACHE': True,
     'AUTORELOAD_IGNORE_CACHE': False,
     'WRITE_SELECTED': [],
-    'PANDOC_TEMPLATES_PATH': ['pandoc_templates'],
     }
 
 PYGMENTS_RST_OPTIONS = None
