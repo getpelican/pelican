780ccfe137a910b404d651a4973f68c85adf01f2
Justin Mayer <entroP@gmail.com>
2015-06-15 13:46:17 -0700

    Prepare version 3.6.1.dev for next development cycle

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CHANGELOG = open('docs/changelog.rst').read()
 
 setup(
     name="pelican",
-    version="3.6.0",
+    version="3.6.1.dev",
     url='http://getpelican.com/',
     author='Alexis Metaireau',
     author_email='authors@getpelican.com',
