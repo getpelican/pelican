a926acf8f36eebcb9a033d7ed2b6c21f44e2f0f7
Lionel Panhaleux <lionel.panhaleux@polyconseil.fr>
2015-08-14 18:23:10 +0200

    Add option to let pelican CLI run as server
    The -S/--serve option is compatible with the -r/--autoreload option.
    Now we need only one processes to serve an autoreloaded site when developing.

--- pelican/server.py
+++ pelican/server.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
 import os
+import os.path
+import posixpath
 import sys
 import logging
+
+
+import threading
+from six.moves.urllib import parse as url_parse
 
 from six.moves import SimpleHTTPServer as srvmod
 from six.moves import socketserver
@@ -14,18 +20,23 @@ except ImportError:
 
 class ComplexHTTPRequestHandler(srvmod.SimpleHTTPRequestHandler):
     SUFFIXES = ['', '.html', '/index.html']
+    SERVPATH = ''
+
+    def translate_path(self, path):
+        """Add the ability to serve a file from a given directory (not cwd)
+        """
+        path = super(ComplexHTTPRequestHandler, self).translate_path(path)
+        return os.path.join(self.SERVPATH, os.path.relpath(path, os.getcwd()))
 
     def do_GET(self):
+        self.original_path = self.path
         # Try to detect file by applying various suffixes
         for suffix in self.SUFFIXES:
-            if not hasattr(self, 'original_path'):
-                self.original_path = self.path
-
             self.path = self.original_path + suffix
             path = self.translate_path(self.path)
 
             if os.path.exists(path):
-                srvmod.SimpleHTTPRequestHandler.do_GET(self)
+                super(ComplexHTTPRequestHandler, self).do_GET()
                 logging.info("Found `%s`." % self.path)
                 break
 
@@ -48,21 +59,36 @@ class ComplexHTTPRequestHandler(srvmod.SimpleHTTPRequestHandler):
         return mimetype
 
 
-if __name__ == '__main__':
-    PORT = len(sys.argv) in (2, 3) and int(sys.argv[1]) or 8000
-    SERVER = len(sys.argv) == 3 and sys.argv[2] or ""
-
-    socketserver.TCPServer.allow_reuse_address = True
+def serve(path, server, port):
+    ComplexHTTPRequestHandler.SERVPATH = path
     try:
-        httpd = socketserver.TCPServer((SERVER, PORT), ComplexHTTPRequestHandler)
+        httpd = socketserver.TCPServer((server, port), ComplexHTTPRequestHandler)
+        httpd.allow_reuse_address = True
     except OSError as e:
-        logging.error("Could not listen on port %s, server %s.", PORT, SERVER)
+        logging.error("Could not listen on port %s, server %s.", port, server)
         sys.exit(getattr(e, 'exitcode', 1))
 
-
-    logging.info("Serving at port %s, server %s.", PORT, SERVER)
+    logging.info("Serving %s at port %s, server %s.", path, port, server)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt as e:
         logging.info("Shutting down server.")
         httpd.socket.close()
+        raise
+
+
+class ServeThread(threading.Thread):
+    def __init__(self, path, server, port):
+        super(ServeThread, self).__init__()
+        self.path = path
+        self.server = server
+        self.port = port
+
+    def run(self):
+        serve(self.path, self.server, self.port)
+
+
+if __name__ == '__main__':
+    PORT = len(sys.argv) in (2, 3) and int(sys.argv[1]) or 8000
+    SERVER = len(sys.argv) == 3 and sys.argv[2] or ""
+    serve('', SERVER, PORT)
