61c371b27ea5b019aa7552219f6c7f671e7ac467
derwinlu <derwinlu+git@gmail.com>
2015-10-19 19:26:31 +0200

    Add 3.5 to setup.py tags

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.3',
          'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
          'Topic :: Internet :: WWW/HTTP',
          'Topic :: Software Development :: Libraries :: Python Modules',
     ],
