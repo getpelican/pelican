46f03589359ab74a7d667aef5bb2a2c33d00135d
Patrick <W7PEA@users.noreply.github.com>
2016-01-20 13:25:53 -0800

    add --markup back to the file
    Added --markup back and removed duplicate --output

--- pelican/tools/pelican_import.py
+++ pelican/tools/pelican_import.py
@@ -787,13 +787,13 @@ def main():
         '--feed', action='store_true', dest='feed',
         help='Feed to parse')
     parser.add_argument(
-        '-o', '--output', dest='output', default='output',
-        help='Output path')
-    parser.add_argument(
         '-o', '--output', dest='output', default='content',
         help=('Where to output the generated markup files. If not specified, '
               'a directory will be created, named "content" in the current '
               'path.'))
+    parser.add_argument(
+        '-m', '--markup', dest='markup', default='rst',
+        help='Output markup format (supports rst & markdown)')
     parser.add_argument(
         '--dir-cat', action='store_true', dest='dircat',
         help='Put files in directories with categories name')
