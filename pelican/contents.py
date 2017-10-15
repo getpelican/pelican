26e77014a35df7d0dc455e24b78ba34bc85f4db0
Charles Flèche <charles.fleche@free.fr>
2017-10-15 13:00:34 +0700

    Do not call refresh_metadata_intersite_links in __init__ if not needed
    This avoid a warning while resolving intersite links if metadata has empty filenames

--- pelican/contents.py
+++ pelican/contents.py
@@ -140,7 +140,9 @@ class Content(object):
         if not hasattr(self, 'status'):
             self.status = getattr(self, 'default_status', None)
 
-        self.refresh_metadata_intersite_links()
+        if len(self._context.get('filenames', [])) > 0:
+            self.refresh_metadata_intersite_links()
+
         signals.content_object_init.send(self)
 
     def __str__(self):
