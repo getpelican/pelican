getpelican/pelican

commit 3a0add4b6e66fe08e9f5710f98235491c09e4f81

Convert FEED settings from `%s` to `{slug}` redux (#2432)
* Convert FEED settings from `%s` to `{slug}` redux

Closes #2106, Closes #2383

pelican/settings.py
@@ -45,10 +45,10 @@ DEFAULT_CONFIG = {
     'THEME_STATIC_DIR': 'theme',
     'THEME_STATIC_PATHS': ['static', ],
     'FEED_ALL_ATOM': posix_join('feeds', 'all.atom.xml'),
-    'CATEGORY_FEED_ATOM': posix_join('feeds', '%s.atom.xml'),
-    'AUTHOR_FEED_ATOM': posix_join('feeds', '%s.atom.xml'),
-    'AUTHOR_FEED_RSS': posix_join('feeds', '%s.rss.xml'),
-    'TRANSLATION_FEED_ATOM': posix_join('feeds', 'all-%s.atom.xml'),
+    'CATEGORY_FEED_ATOM': posix_join('feeds', '{slug}.atom.xml'),
+    'AUTHOR_FEED_ATOM': posix_join('feeds', '{slug}.atom.xml'),
+    'AUTHOR_FEED_RSS': posix_join('feeds', '{slug}.rss.xml'),
+    'TRANSLATION_FEED_ATOM': posix_join('feeds', 'all-{lang}.atom.xml'),
     'FEED_MAX_ITEMS': '',
     'RSS_FEED_SUMMARY_ONLY': True,
     'SITEURL': '',
@@ -384,6 +384,26 @@ def handle_deprecated_settings(settings):
                     ]
                 settings[f + '_REGEX_SUBSTITUTIONS'] = regex_subs
             settings.pop(f + '_SUBSTITUTIONS', None)
+
+    # `%s` -> '{slug}` or `{lang}` in FEED settings
+        for key in ['TRANSLATION_FEED_ATOM',
+                    'TRANSLATION_FEED_RSS'
+                    ]:
+            if key in settings and '%s' in settings[key]:
+                logger.warning('%%s usage in %s is deprecated, use {lang} '
+                               'instead. Falling back to default.', key)
+                settings[key] = DEFAULT_CONFIG[key]
+        for key in ['AUTHOR_FEED_ATOM',
+                    'AUTHOR_FEED_RSS',
+                    'CATEGORY_FEED_ATOM',
+                    'CATEGORY_FEED_RSS',
+                    'TAG_FEED_ATOM',
+                    'TAG_FEED_RSS',
+                    ]:
+            if key in settings and '%s' in settings[key]:
+                logger.warning('%%s usage in %s is deprecated, use {slug} '
+                               'instead. Falling back to default.', key)
+                settings[key] = DEFAULT_CONFIG[key]
 
     return settings
 

THANKS
@@ -155,6 +155,7 @@ Tshepang Lekhonkhobe
 Valentin-Costel Hăloiu
 Vlad Niculae
 William Light
+William Minchin
 Wladislaw Merezhko
 W. Trevor King
 Zoresvit

pelican/generators.py
@@ -302,17 +302,21 @@ class ArticlesGenerator(CachingGenerator):
         """Generate the feeds from the current context, and output files."""
 
         if self.settings.get('FEED_ATOM'):
-            writer.write_feed(self.articles, self.context,
-                              self.settings['FEED_ATOM'],
-                              self.settings.get('FEED_ATOM_URL',
-                                                self.settings['FEED_ATOM']))
+            writer.write_feed(
+                self.articles,
+                self.context,
+                self.settings['FEED_ATOM'],
+                self.settings.get('FEED_ATOM_URL', self.settings['FEED_ATOM'])
+                )
 
         if self.settings.get('FEED_RSS'):
-            writer.write_feed(self.articles, self.context,
-                              self.settings['FEED_RSS'],
-                              self.settings.get('FEED_RSS_URL',
-                                                self.settings['FEED_RSS']),
-                              feed_type='rss')
+            writer.write_feed(
+                self.articles,
+                self.context,
+                self.settings['FEED_RSS'],
+                self.settings.get('FEED_RSS_URL', self.settings['FEED_RSS']),
+                feed_type='rss'
+                )
 
         if (self.settings.get('FEED_ALL_ATOM') or
                 self.settings.get('FEED_ALL_RSS')):
@@ -323,80 +327,105 @@ class ArticlesGenerator(CachingGenerator):
                           order_by=self.settings['ARTICLE_ORDER_BY'])
 
             if self.settings.get('FEED_ALL_ATOM'):
-                writer.write_feed(all_articles, self.context,
-                                  self.settings['FEED_ALL_ATOM'],
-                                  self.settings.get(
-                                      'FEED_ALL_ATOM_URL',
-                                      self.settings['FEED_ALL_ATOM']))
+                writer.write_feed(
+                    all_articles,
+                    self.context,
+                    self.settings['FEED_ALL_ATOM'],
+                    self.settings.get('FEED_ALL_ATOM_URL',
+                                      self.settings['FEED_ALL_ATOM'])
+                    )
 
             if self.settings.get('FEED_ALL_RSS'):
-                writer.write_feed(all_articles, self.context,
-                                  self.settings['FEED_ALL_RSS'],
-                                  self.settings.get(
-                                      'FEED_ALL_RSS_URL',
+                writer.write_feed(
+                    all_articles,
+                    self.context,
+                    self.settings['FEED_ALL_RSS'],
+                    self.settings.get('FEED_ALL_RSS_URL',
                                       self.settings['FEED_ALL_RSS']),
-                                  feed_type='rss')
+                    feed_type='rss'
+                    )
 
         for cat, arts in self.categories:
             if self.settings.get('CATEGORY_FEED_ATOM'):
-                writer.write_feed(arts, self.context,
-                                  self.settings['CATEGORY_FEED_ATOM']
-                                  % cat.slug,
-                                  self.settings.get(
-                                      'CATEGORY_FEED_ATOM_URL',
-                                      self.settings['CATEGORY_FEED_ATOM'])
-                                  % cat.slug, feed_title=cat.name)
+                writer.write_feed(
+                    arts,
+                    self.context,
+                    self.settings['CATEGORY_FEED_ATOM'].format(slug=cat.slug),
+                    self.settings.get(
+                        'CATEGORY_FEED_ATOM_URL',
+                        self.settings['CATEGORY_FEED_ATOM']).format(
+                            slug=cat.slug
+                        ),
+                    feed_title=cat.name
+                    )
 
             if self.settings.get('CATEGORY_FEED_RSS'):
-                writer.write_feed(arts, self.context,
-                                  self.settings['CATEGORY_FEED_RSS']
-                                  % cat.slug,
-                                  self.settings.get(
-                                      'CATEGORY_FEED_RSS_URL',
-                                      self.settings['CATEGORY_FEED_RSS'])
-                                  % cat.slug, feed_title=cat.name,
-                                  feed_type='rss')
+                writer.write_feed(
+                    arts,
+                    self.context,
+                    self.settings['CATEGORY_FEED_RSS'].format(slug=cat.slug),
+                    self.settings.get(
+                        'CATEGORY_FEED_RSS_URL',
+                        self.settings['CATEGORY_FEED_RSS']).format(
+                            slug=cat.slug
+                        ),
+                    feed_title=cat.name,
+                    feed_type='rss'
+                    )
 
         for auth, arts in self.authors:
             if self.settings.get('AUTHOR_FEED_ATOM'):
-                writer.write_feed(arts, self.context,
-                                  self.settings['AUTHOR_FEED_ATOM']
-                                  % auth.slug,
-                                  self.settings.get(
-                                      'AUTHOR_FEED_ATOM_URL',
-                                      self.settings['AUTHOR_FEED_ATOM'])
-                                  % auth.slug, feed_title=auth.name)
+                writer.write_feed(
+                    arts,
+                    self.context,
+                    self.settings['AUTHOR_FEED_ATOM'].format(slug=auth.slug),
+                    self.settings.get(
+                        'AUTHOR_FEED_ATOM_URL',
+                        self.settings['AUTHOR_FEED_ATOM']
+                        ).format(slug=auth.slug),
+                    feed_title=auth.name
+                    )
 
             if self.settings.get('AUTHOR_FEED_RSS'):
-                writer.write_feed(arts, self.context,
-                                  self.settings['AUTHOR_FEED_RSS']
-                                  % auth.slug,
-                                  self.settings.get(
-                                      'AUTHOR_FEED_RSS_URL',
-                                      self.settings['AUTHOR_FEED_RSS'])
-                                  % auth.slug, feed_title=auth.name,
-                                  feed_type='rss')
+                writer.write_feed(
+                    arts,
+                    self.context,
+                    self.settings['AUTHOR_FEED_RSS'].format(slug=auth.slug),
+                    self.settings.get(
+                        'AUTHOR_FEED_RSS_URL',
+                        self.settings['AUTHOR_FEED_RSS']
+                        ).format(slug=auth.slug),
+                    feed_title=auth.name,
+                    feed_type='rss'
+                    )
 
         if (self.settings.get('TAG_FEED_ATOM') or
                 self.settings.get('TAG_FEED_RSS')):
             for tag, arts in self.tags.items():
                 if self.settings.get('TAG_FEED_ATOM'):
-                    writer.write_feed(arts, self.context,
-                                      self.settings['TAG_FEED_ATOM']
-                                      % tag.slug,
-                                      self.settings.get(
-                                          'TAG_FEED_ATOM_URL',
-                                          self.settings['TAG_FEED_ATOM'])
-                                      % tag.slug, feed_title=tag.name)
+                    writer.write_feed(
+                        arts,
+                        self.context,
+                        self.settings['TAG_FEED_ATOM'].format(slug=tag.slug),
+                        self.settings.get(
+                            'TAG_FEED_ATOM_URL',
+                            self.settings['TAG_FEED_ATOM']
+                            ).format(slug=tag.slug),
+                        feed_title=tag.name
+                        )
 
                 if self.settings.get('TAG_FEED_RSS'):
-                    writer.write_feed(arts, self.context,
-                                      self.settings['TAG_FEED_RSS'] % tag.slug,
-                                      self.settings.get(
-                                          'TAG_FEED_RSS_URL',
-                                          self.settings['TAG_FEED_RSS'])
-                                      % tag.slug, feed_title=tag.name,
-                                      feed_type='rss')
+                    writer.write_feed(
+                        arts,
+                        self.context,
+                        self.settings['TAG_FEED_RSS'].format(slug=tag.slug),
+                        self.settings.get(
+                            'TAG_FEED_RSS_URL',
+                            self.settings['TAG_FEED_RSS']
+                            ).format(slug=tag.slug),
+                        feed_title=tag.name,
+                        feed_type='rss'
+                        )
 
         if (self.settings.get('TRANSLATION_FEED_ATOM') or
                 self.settings.get('TRANSLATION_FEED_RSS')):
@@ -409,19 +438,27 @@ class ArticlesGenerator(CachingGenerator):
                     items, order_by=self.settings['ARTICLE_ORDER_BY'])
                 if self.settings.get('TRANSLATION_FEED_ATOM'):
                     writer.write_feed(
-                        items, self.context,
-                        self.settings['TRANSLATION_FEED_ATOM'] % lang,
+                        items,
+                        self.context,
+                        self.settings['TRANSLATION_FEED_ATOM']
+                            .format(lang=lang),
                         self.settings.get(
                             'TRANSLATION_FEED_ATOM_URL',
-                            self.settings['TRANSLATION_FEED_ATOM']) % lang)
+                            self.settings['TRANSLATION_FEED_ATOM']
+                            ).format(lang=lang),
+                        )
                 if self.settings.get('TRANSLATION_FEED_RSS'):
                     writer.write_feed(
-                        items, self.context,
-                        self.settings['TRANSLATION_FEED_RSS'] % lang,
+                        items,
+                        self.context,
+                        self.settings['TRANSLATION_FEED_RSS']
+                            .format(lang=lang),
                         self.settings.get(
                             'TRANSLATION_FEED_RSS_URL',
-                            self.settings['TRANSLATION_FEED_RSS']) % lang,
-                        feed_type='rss')
+                            self.settings['TRANSLATION_FEED_RSS']
+                            ).format(lang=lang),
+                        feed_type='rss'
+                        )
 
     def generate_articles(self, write):
         """Generate the articles."""

pelican/tests/default_conf.py
@@ -12,7 +12,7 @@ REVERSE_CATEGORY_ORDER = True
 DEFAULT_PAGINATION = 2
 
 FEED_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 LINKS = (('Biologeek', 'http://biologeek.org'),
          ('Filyb', "http://filyb.info/"),

pelican/tools/templates/publishconf.py.jinja2
@@ -15,7 +15,7 @@ SITEURL = '{{siteurl}}'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 

samples/pelican.conf.py
@@ -18,7 +18,7 @@ DEFAULT_PAGINATION = 4
 DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 LINKS = (('Biologeek', 'http://biologeek.org'),
          ('Filyb', "http://filyb.info/"),

samples/pelican.conf_FR.py
@@ -22,7 +22,7 @@ ARTICLE_URL = 'posts/{date:%Y}/{date:%B}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 LINKS = (('Biologeek', 'http://biologeek.org'),
          ('Filyb', "http://filyb.info/"),

commit b1d44c1c871acee8687b93ba34d6354f1a56d63f

Replace %s rather than fallback to defaults
3a0add4b6e66fe08e9f5710f98235491c09e4f81 caused existing configs to fall
back to defaults. But since we know exactly how to fix the user config
so that the behavior doesn't change, we should do so, while still
warning that use of %s is deprecated.

Also fixes a bug where we tried to look for %s in None.

pelican/settings.py
@@ -245,6 +245,22 @@ def get_jinja_environment(settings):
     return settings
 
 
+def _printf_s_to_format_field(printf_string, format_field):
+    """Tries to replace %s with {format_field} in the provided printf_string.
+    Raises ValueError in case of failure.
+    """
+    TEST_STRING = 'PELICAN_PRINTF_S_DEPRECATION'
+    expected = printf_string % TEST_STRING
+
+    result = printf_string.replace('{', '{{').replace('}', '}}') \
+        % '{{{}}}'.format(format_field)
+    if result.format(**{format_field: TEST_STRING}) != expected:
+        raise ValueError('Failed to safely replace %s with {{{}}}'.format(
+            format_field))
+
+    return result
+
+
 def handle_deprecated_settings(settings):
     """Converts deprecated settings and issues warnings. Issues an exception
     if both old and new setting is specified.
@@ -394,10 +410,16 @@ def handle_deprecated_settings(settings):
     for key in ['TRANSLATION_FEED_ATOM',
                 'TRANSLATION_FEED_RSS'
                 ]:
-        if key in settings and '%s' in settings[key]:
+        if settings.get(key) and '%s' in settings[key]:
             logger.warning('%%s usage in %s is deprecated, use {lang} '
-                           'instead. Falling back to default.', key)
-            settings[key] = DEFAULT_CONFIG[key]
+                           'instead.', key)
+            try:
+                settings[key] = _printf_s_to_format_field(
+                    settings[key], 'lang')
+            except ValueError:
+                logger.warning('Failed to convert %%s to {lang} for %s. '
+                               'Falling back to default.', key)
+                settings[key] = DEFAULT_CONFIG[key]
     for key in ['AUTHOR_FEED_ATOM',
                 'AUTHOR_FEED_RSS',
                 'CATEGORY_FEED_ATOM',
@@ -405,10 +427,16 @@ def handle_deprecated_settings(settings):
                 'TAG_FEED_ATOM',
                 'TAG_FEED_RSS',
                 ]:
-        if key in settings and '%s' in settings[key]:
+        if settings.get(key) and '%s' in settings[key]:
             logger.warning('%%s usage in %s is deprecated, use {slug} '
-                           'instead. Falling back to default.', key)
-            settings[key] = DEFAULT_CONFIG[key]
+                           'instead.', key)
+            try:
+                settings[key] = _printf_s_to_format_field(
+                    settings[key], 'slug')
+            except ValueError:
+                logger.warning('Failed to convert %%s to {slug} for %s. '
+                               'Falling back to default.', key)
+                settings[key] = DEFAULT_CONFIG[key]
 
     return settings
 

pelican/tests/test_settings.py
@@ -9,6 +9,7 @@ from sys import platform
 
 
 from pelican.settings import (DEFAULT_CONFIG, DEFAULT_THEME,
+                              _printf_s_to_format_field,
                               configure_settings, handle_deprecated_settings,
                               read_settings)
 from pelican.tests.support import unittest
@@ -167,6 +168,14 @@ class TestSettingsConfiguration(unittest.TestCase):
         settings['THEME'] = 'foo'
 
         self.assertRaises(Exception, configure_settings, settings)
+
+    def test__printf_s_to_format_field(self):
+        for s in ('%s', '{%s}', '{%s'):
+            option = 'foo/{}/bar.baz'.format(s)
+            result = _printf_s_to_format_field(option, 'slug')
+            expected = option % 'qux'
+            found = result.format(slug='qux')
+            self.assertEqual(expected, found)
 
     def test_deprecated_extra_templates_paths(self):
         settings = self.settings