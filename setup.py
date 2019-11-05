ae73d063016d4de013eb3da2d0d6d4d6510cddd7
Paolo Melchiorre <paolo@20tab.com>
2019-11-05 21:04:56 +0100

    Remove Python 2.7 support from settings

--- setup.py
+++ setup.py
@@ -71,8 +71,6 @@ setup(
         'Framework :: Pelican',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
