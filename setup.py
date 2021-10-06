0da8659d0ee5b23550fcf0d2314bb398a00b18a1
Justin Mayer <entroP@gmail.com>
2021-10-06 09:51:00 +0200

    Add Python 3.10 to test matrix & classifier list

--- setup.py
+++ setup.py
@@ -69,6 +69,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
