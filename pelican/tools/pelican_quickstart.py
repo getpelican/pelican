7bd775b9bcc26e58f26ac56db0785bbd8121db9f
Moritz Meier <moritzmeier@aol.com>
2023-06-25 15:15:36 +0200

    Update pelican_quickstart.py
    Fixes bug of missing zone attribute caused by tzlocal 4.0 API change.

--- pelican/tools/pelican_quickstart.py
+++ pelican/tools/pelican_quickstart.py
@@ -16,7 +16,10 @@ except ImportError:
 
 try:
     import tzlocal
-    _DEFAULT_TIMEZONE = tzlocal.get_localzone().zone
+    if hasattr(tzlocal.get_localzone(), "zone"):
+        _DEFAULT_TIMEZONE = tzlocal.get_localzone().zone
+    else:
+        _DEFAULT_TIMEZONE = tzlocal.get_localzone_name()
 except ImportError:
     _DEFAULT_TIMEZONE = 'Europe/Rome'
 
