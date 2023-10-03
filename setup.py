de0fae81821a78ed6b508d3570b56edb35293309
Lioman <dev@lioman.de>
2023-10-03 16:59:57 +0200

    Add python 3.12 to test matrix

--- setup.py
+++ setup.py
@@ -77,6 +77,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
